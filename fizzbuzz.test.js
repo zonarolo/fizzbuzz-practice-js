ce8c23484377e6e979c7d0effd9a4c11cb31b74e
Juan Rolo <juanluis211@outlook.com>
2021-05-25 00:43:46 +0200

    Add buzz and fizzbuzz test

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -19,4 +19,21 @@ describe('fizzbuzz', () => {
     expect(expected).toBe(result);
   });
 
+  test("should print buzz if they receive 5", () => {
+    const expected = 'buzz';
+    const result = fizzbuzz(5);
+    expect(expected).toBe(result);
+  });
+
+  test("should print buzz if they receive a multiple of 5", () => {
+    const expected = "buzz";
+    const result = fizzbuzz(10);
+    expect(expected).toBe(result);
+  });
+
+  test("should print fizzbuzz if they receive a multiple of 3 and 5", () => {
+    const expected = "fizzbuzz";
+    const result = fizzbuzz(15);
+    expect(expected).toBe(result);
+  });
 });
