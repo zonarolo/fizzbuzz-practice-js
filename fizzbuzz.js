2e32369e1586e00d5b6b1f5c25c09717204c9cdd
Juan Rolo <juanluis211@outlook.com>
2021-05-25 00:36:21 +0200

    fizz

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+function fizzbuzz(num) {
+  
+  if (num % 3 === 0){
+    return "fizz";
+  }
+
+  return num;
+}
+
+module.exports = fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,22 @@
+const fizzbuzz = require("./fizzbuzz");
+
 describe('fizzbuzz', () => {
-  test('test', () => {
-    expect(true).toBe(true);
+  test('should print 1 if they receive 1', () => {
+    const expected = 1;
+    const result = fizzbuzz(1);
+    expect(expected).toBe(result);
   });
+
+  test("should print fizz if they receive 3", () => {
+    const expected = 'fizz';
+    const result = fizzbuzz(3);
+    expect(expected).toBe(result);
+  });
+
+  test("should print fizz if they receive a multiple of 3", () => {
+    const expected = "fizz";
+    const result = fizzbuzz(6);
+    expect(expected).toBe(result);
+  });
+
 });
